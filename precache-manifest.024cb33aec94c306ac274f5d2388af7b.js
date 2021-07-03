6f0fd05c9b547fc6bbce5e4f61d3db8612b9050f
maamoun toj <maamoun.toj@gmail.com>
2021-07-03 15:01:23 +0100

    Updates

---
+++ precache-manifest.024cb33aec94c306ac274f5d2388af7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3527aec08bc7c1cff765cda17297f3df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "735c754705ac773b4e33",
+    "url": "./static/css/main.e1b2793c.chunk.css"
+  },
+  {
+    "revision": "99153ed33e0fdf6e79c4",
+    "url": "./static/js/2.4ac9cb30.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.4ac9cb30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735c754705ac773b4e33",
+    "url": "./static/js/main.2476fdec.chunk.js"
+  },
+  {
+    "revision": "2637c33404482adb5812",
+    "url": "./static/js/runtime-main.51560261.js"
+  }
+]);
